a0f752dccb0e1a4c842e6b774912e271c9f63be0
Raulpassos08 <raulfera2@gmail.com>
2024-12-12 14:55:27 -0300

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,9 +4,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { TodolistComponent } from './todolist/todolist.component';
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TodolistComponent
   ],
   imports: [
     BrowserModule,
